b7dfdc7f9add9fc967e44b2f4e290a68eb37d3f7
Tyson Brown <90238566+tysonpbr@users.noreply.github.com>
2024-10-23 15:23:18 -0700

    pushing trained model

--- predict.py
+++ predict.py
@@ -3,9 +3,10 @@ from ultralytics import YOLO
 import cv2
 
 
-model_path = "C:/Users/tyson/Desktop/image-segmentation-yolov8/yolov8n-seg.pt"
+model_path = "C:/Users/tyson/Desktop/tree-segmentation/runs/segment/train/weights/last.pt"
 
-image_path = "C:/Users/tyson/Desktop/image-segmentation-yolov8/test/red_maple3.jpg"
+# image_path = "C:/Users/tyson/Desktop/tree-segmentation/test/red_maple3.jpg"
+image_path = "C:/Users/tyson/Desktop/tree-segmentation/data/images/val/0.jpg"
 
 img = cv2.imread(image_path)
 H, W, _ = img.shape
@@ -15,11 +16,14 @@ model = YOLO(model_path)
 results = model(img)
 
 for result in results:
+  if result.masks:
     for j, mask in enumerate(result.masks.data):
 
-        mask = mask.numpy() * 255
+      mask = mask.numpy() * 255
 
-        mask = cv2.resize(mask, (W, H))
+      mask = cv2.resize(mask, (W, H))
 
-        cv2.imwrite('./output.png', mask)
+      cv2.imwrite('./test/output.png', mask)
+  else:
+    print("No data")
 
