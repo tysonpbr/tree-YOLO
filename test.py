0119f3656ff8f3575b6621d3d215539be225f1cb
tysonpbr <thetysonbrown@gmail.com>
2024-10-23 17:51:39 -0700

    added cropping for tree trunks

--- test.py
+++ test.py
@@ -9,8 +9,38 @@ model_path = "C:/Users/tyson/Desktop/tree-segmentation/runs/segment/train/weight
 images_dir = "C:/Users/tyson/Desktop/tree-segmentation/test/images"
 masks_output_dir = "./test/output/masks/"
 combined_output_dir = "./test/output/combined/"
+cropped_output_dir = "./test/output/cropped/"
 
 model = YOLO(model_path)
+
+def find_largest_white_rectangle(mask):
+    rows, cols = mask.shape
+
+    height = np.zeros((rows, cols), dtype=int)
+
+    for r in range(rows):
+        for c in range(cols):
+            if mask[r, c] != 0:
+                height[r, c] = height[r - 1, c] + 1 if r > 0 else 1
+
+    max_area = 0
+    best_coords = (0, 0, 0, 0)
+
+    for r in range(rows):
+        stack = []
+        for c in range(cols + 1):
+            h = height[r, c] if c < cols else 0 
+            while stack and h < height[r, stack[-1]]:
+                H = height[r, stack.pop()]
+                W = c if not stack else c - stack[-1] - 1
+                area = H * W
+                if area > max_area:
+                    max_area = area
+                    x = stack[-1] + 1 if stack else 0
+                    best_coords = (x, r - H + 1, W, H)
+            stack.append(c)
+
+    return best_coords
 
 for filename in os.listdir(images_dir):    
   if filename.endswith((".jpg", ".jpeg")):
@@ -37,6 +67,11 @@ for filename in os.listdir(images_dir):
           binary_mask = (mask > 128).astype(np.uint8)
           masked_image = cv2.bitwise_and(img, img, mask=binary_mask)
           cv2.imwrite(combined_output_dir + filename, masked_image)
+
+          largest_rect = find_largest_white_rectangle(binary_mask)
+          x, y, w, h = largest_rect
+          cropped_image = img[y:y+h, x:x+w]
+          cv2.imwrite(cropped_output_dir + filename, cropped_image)
 
 
 
